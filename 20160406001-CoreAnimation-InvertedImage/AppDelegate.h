c9999796ee3825abb6e1b1f8a315652495de984b
raineryue <it_jinlei.yue@163.com>
2016-04-06 11:22:17 +0800

    Initial Commit

---
+++ 20160406001-CoreAnimation-InvertedImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  20160406001-CoreAnimation-InvertedImage
+//
+//  Created by Rainer on 16/4/6.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
